15ef3e8a2efc07c6acd7c31d0382da8532c02e1e
Jorge Hinojosa <jorgexhinojosa@gmail.com>
2019-03-26 14:03:39 -0500

    Finished React Drills Set 1

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      food: ['burgers', 'pizza', 'wings', 'pasta', 'hot dogs'],
+    }
+  }
+
   render() {
+    let foodList = this.state.food.map((e) => <h2>{e}</h2>);
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {foodList}
       </div>
     );
   }
--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      food: ['tacos', 'enchiladas', 'nachos', 'quesadillas', 'sopes'],
+      userInput: '',
+    };
+  }
+  handleChange(val) {
+    this.setState({userInput: val});
+  }
   render() {
+    let foodList = this.state.food
+      .filter((e, i) => {
+        return e.includes(this.state.userInput)
+      }) 
+      .map((e, i) => <h2>{e}</h2>)
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={(event) => this.handleChange(event.target.value)} />
+        {foodList}
       </div>
     );
   }
